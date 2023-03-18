6bce4ea9899918af9eb056d7627ca17d295f19b4
Michelle Chalupnik <mvchalupnik@gmail.com>
2023-03-17 23:50:57 -0400

    test

--- target/org.transcrypt.__runtime__.js
+++ target/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2023-03-17 23:31:42
+// Transcrypt'ed from Python, 2023-03-17 23:46:55
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
